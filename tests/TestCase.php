a1f3eee24203f821a320f090344051980df52209
freelancerfarhad <forhadhossainfh22@gmailcom>
2022-05-02 11:52:00 +0600

    notun kicu

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,3 +8,19 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 }
+
+
+
+
+//  next time jokhon ei folder commuit korben tokhon
+
+
+// git add .
+//**enter */
+
+// gii commit -m" your memessage"
+
+//**enter */
+
+
+// git push -u origin <branceName>    i.e. $ git push -u origin master    OR   $ git push -u origin main etc
